5b0ad708f9adc28484bb4a5c1bdd01b4a01bc3ac
Karl Prior <karlprior@hotmail.com>
2022-04-30 06:43:00 +0100

    linted files

--- synchroniser.py
+++ synchroniser.py
@@ -1,22 +1,24 @@
 from appservice import (
-  AppService,
-  Query,
-  QueryResponse
+    AppService,
+    Query,
+    QueryResponse
 )
 
 import constants
 
+
 class GetSchemaResponse(QueryResponse):
-  def GetName():
-    pass
+    def GetName():
+        pass
+
 
 class GetSchemaQuery(Query):
-  def GetResponse() -> GetSchemaResponse:
-    pass
-  
+    def GetResponse() -> GetSchemaResponse:
+        pass
+
+
 def main():
-  app_service = AppService()
-  response: GetSchemaResponse = app_service.Query(GetSchemaQuery())  
-  response.GetName()
-  print(constants.FROM_WEBFLOW_ACCESS_TOKEN)
-  
+    app_service = AppService()
+    response: GetSchemaResponse = app_service.Query(GetSchemaQuery())
+    response.GetName()
+    print(constants.FROM_WEBFLOW_ACCESS_TOKEN)
